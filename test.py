07137238ead7742778b64388f48e04ea35827552
forwiat <forwiat@163.com>
2019-11-07 12:04:40 +0800

    change test.py

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
 from duration_model import Duration_Graph
 from acoustic_model import Acoustic_Graph
+from hyperparams import hyperparams
+hp = hyperparams()
 def main():
     mode = 'test'
-    dur_train_graph = Duration_Graph(mode='train')
-    print(f'Duration {mode} Graph loaded done.')
-    try:
-        while 1:
-            y_hat, loss, steps = dur_train_graph.train()
-            print('#-------------------------Duration Graph train-------------------------#')
-            print(f'#-------------------------steps : {steps} \t loss : {loss}-------------------------#')
-    except:
-        print('Duration Graph Train over.')
+    if hp.TEST_GRAPH.lower() is 'duration':
+        dur_test_graph = Duration_Graph(mode=mode)
+        print(f'{hp.TEST_GRAPH} {mode} Graph loaded done.')
+        try:
+            while 1:
+                _, loss, steps = dur_test_graph.test()
+                print('#-------------------------Duration Graph Test-------------------------#')
+                print(f'#-------------------------steps : {steps} \t loss : {loss}-------------------------#')
+        except:
+            print('Duration Graph Test done.')
+    elif hp.TEST_GRAPH.lower() is 'acoustic':
+        syn_test_graph = Acoustic_Graph(mode=mode)
+        print(f'{hp.TEST_GRAPH} {mode} Graph loaded done.')
+        try:
+            while 1:
+                _, loss, steps = syn_test_graph.test()
+                print('#-------------------------Acoustic Graph Test-------------------------#')
+                print(f'#-------------------------steps : {steps} \t loss : {loss}-------------------------#')
+        except:
+            print('Acoustic Graph Test done.')
+    else:
+        raise Exception(f'No supported TEST_GRAPH named {hp.TEST_GRAPH}. Please check.')
 
 if __name__ == '__main__':
     main()
